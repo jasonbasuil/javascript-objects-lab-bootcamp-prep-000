8a1d3c62a7e83ab23d4431c47ef0a6c8453629e5
Jason Basuil <basuilj@gmail.com>
2018-10-01 19:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function updateObjectWithKeyAndValue (object, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
+
+function deleteObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteObjectWithKeyAndValue(object, key){
+  
+}

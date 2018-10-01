56740891773ebb4ca1e922aad11e72d56c6b3287
Jason Basuil <basuilj@gmail.com>
2018-10-01 19:15:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue (object, key, value){
+  var newObj = Object.assign({key:value}, object)
+  return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
